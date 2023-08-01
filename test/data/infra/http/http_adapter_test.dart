6895d268ce439aed55d3f85884e959f150b0eb1f
Maya <mayannara@outlook.com>
2023-08-01 11:32:55 -0300

    Ensure HttpAdapter calls post with correct Body

--- test/data/infra/http/http_adapter_test.dart
+++ test/data/infra/http/http_adapter_test.dart
@@ -1,6 +1,7 @@
-import 'dart:html';
+//Lib recebe o body em formato de string, tem que converter para o Json
+import 'dart:convert';
 
-import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
 
 class HttpAdapter {
   final Client client;
@@ -9,23 +10,42 @@ class HttpAdapter {
 
   Future<void> request({
     @required String url,
-    @required String method
+    @required String method,
+    Map body
   }) async {
-
+      final headers = {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      };
+    await client.post(url, headers: headers, body: jsonEncode(body));
   }
 }
 
 class ClientSpy extends Mock implements Client {}
 
 void main() {
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
+
   group('post', () {
     test('Should call post with correct values', () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
+      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'} );
 
-      await sut.request(url: url, method: 'post');
-
-      verify(client.post(url));
+      verify(client.post(
+        url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+        },
+        body: '{"any_key": "any_key"}'
+      ));
     });
   });
 }
