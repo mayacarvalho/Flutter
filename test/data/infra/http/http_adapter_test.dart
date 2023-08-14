888b39f656ef1ca23f2f285a7b78f358169d4b0e
Maya <mayannara@outlook.com>
2023-08-14 16:21:44 -0300

    Ensure HttpAdapter returns null if post returns 204

--- test/data/infra/http/http_adapter_test.dart
+++ test/data/infra/http/http_adapter_test.dart
@@ -21,7 +21,11 @@ class HttpAdapter implements HttpClient {
       };
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+    if (response.statusCode == 200){
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else{
+      return null;
+    }
   }
 }
 
@@ -88,6 +92,26 @@ void main() {
   group('post', () {
     test('Should return null if post returns 200 with no data', () async {
       mockResponse(200, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+  });
+
+  group('post', () {
+    test('Should return null if post returns 204', () async {
+      mockResponse(204, body: '');
+      
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+  });
+
+   group('post', () {
+    test('Should return null if post returns 204 with data', () async {
+      mockResponse(204);
       
       final response = await sut.request(url: url, method: 'post');
 
