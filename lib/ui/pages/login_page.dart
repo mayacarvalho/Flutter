d63c56b278468823e51f1ec0fca7eb00b34e83ba
Maya <mayannara@outlook.com>
2023-08-27 22:38:12 -0300

    Create login page

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,16 +1,50 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Container(
-            child: Image(image: AssetImage('lib/ui/assets/logo.png')),
-          ) // Container
-        ], // <Widget>[]
-      ), // Column
-    ); // SingleChildScrollView
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Image(image: AssetImage('lib/ui/assets/logo.png')),
+            ), // Container
+            Text('Login'.toUpperCase()),
+            Form(
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      icon: Icon(Icons.email)
+                    ), // InputDecoration
+                    keyboardType: TextInputType.emailAddress,
+                  ), // TextFormField
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      icon: Icon(Icons.lock)
+                    ), // InputDecoration
+                    obscureText: True,
+                  ), // TextFormField
+                  RaiseButton(
+                    onPressed: () {},
+                    child:Text('Entrar'.toUpperCase()),
+                  ), // RaiseButton
+                  FlatButton.icon(
+                    onPressed:() {},
+                    icon: Icon(Icons.person),
+                    label: Text('Criar conta')
+                  ) // FlatButton.icon
+                ],
+              ), // Column
+            ), // Form
+          ], // <Widget>[]
+        ), // Column
+      ) // SingleChildScrollView
+    );
   }
 }
