992215fc5d0a6eff97b7c7f4ffd98169405a974b
Maya <mayannara@outlook.com>
2023-08-28 00:03:32 -0300

    Add icon for app

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,50 +1,63 @@
-import 'dart:html';
+import '../../components/components.dart';
+import '../../helpers/helpers.dart';
+import '../../mixins/mixins.dart';
+import './components/components.dart';
+import './login.dart';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatelessWidget with KeyboardManager, LoadingManager, UIErrorManager, NavigationManager {
+  final LoginPresenter presenter;
+
+  LoginPage(this.presenter);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Image(image: AssetImage('lib/ui/assets/logo.png')),
-            ), // Container
-            Text('Login'.toUpperCase()),
-            Form(
+      body: Builder(
+        builder: (context) {
+          handleLoading(context, presenter.isLoadingStream);
+          handleMainError(context, presenter.mainErrorStream);
+          handleNavigation(presenter.navigateToStream, clear: true);
+
+          return GestureDetector(
+            onTap: () => hideKeyboard(context),
+            child: SingleChildScrollView(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      icon: Icon(Icons.email)
-                    ), // InputDecoration
-                    keyboardType: TextInputType.emailAddress,
-                  ), // TextFormField
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Senha',
-                      icon: Icon(Icons.lock)
-                    ), // InputDecoration
-                    obscureText: True,
-                  ), // TextFormField
-                  RaiseButton(
-                    onPressed: () {},
-                    child:Text('Entrar'.toUpperCase()),
-                  ), // RaiseButton
-                  FlatButton.icon(
-                    onPressed:() {},
-                    icon: Icon(Icons.person),
-                    label: Text('Criar conta')
-                  ) // FlatButton.icon
+                  LoginHeader(),
+                  Headline1(text: R.string.login),
+                  Padding(
+                    padding: EdgeInsets.all(32),
+                    child: ListenableProvider(
+                      create: (_) => presenter,
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[
+                            EmailInput(),
+                            Padding(
+                              padding: EdgeInsets.only(top: 8, bottom: 32),
+                              child: PasswordInput(),
+                            ),
+                            LoginButton(),
+                            TextButton.icon(
+                              onPressed: presenter.goToSignUp,
+                              icon: Icon(Icons.person),
+                              label: Text(R.string.addAccount)
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
                 ],
-              ), // Column
-            ), // Form
-          ], // <Widget>[]
-        ), // Column
-      ) // SingleChildScrollView
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
