92e0e5cb1e50d77ca3653d83ee27a773a2148bab
Maya <mayannara@outlook.com>
2023-09-19 16:30:20 -0300

    Organize presentation

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -2,20 +2,8 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-abstract class Validation {
-  validate({
-    @required String field, 
-    @required String value
-  });
-}
-
-class StreamLoginPresenter {
-  final Validation validation;
-
-  StreamLoginPresenter({@required this.validation});
-  void ValidateEmail(String email){
-  }
-}
+import '../../../lib/presentation/presenters/protocols/protocols.dart';
+import '../../../lib/presentation/presenters/presenters.dart';
 
 class ValidationSpy extends Mock implements Validation {}
 
@@ -24,6 +12,13 @@ void main() {
   ValidationSpy validation;
   String email;
   
+  PostExpectation mockValidationCall(String field) =>
+    when(validation.validate(field: field == null ? anyNamed('field'), value: anyNamed('value')));
+  
+  void mockValidation({String field, String value}) {
+    mockValidationCall(field).thenReturn(value);
+  }
+
   setUp(() {
     final validation = ValidationSpy();
     final sut = StreamLoginPresenter(validation: validation);
@@ -33,5 +28,13 @@ void main() {
   test('Should call Validation with correct email', () {
     sut.ValidateEmail(email);
     verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit email error if validation fails', () {
+    mockValidation(value: 'error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.ValidateEmail(email);
   });
 }
