7d49ef5c30eed782a79d6f9210baa36eee8cba39
MayannaraCarvalho <mayannara.carvalho@solvis.com.br>
2023-10-09 16:20:15 -0300

    Ensure StreamLoginPresenter emits null if email is valid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -33,8 +33,17 @@ void main() {
   test('Should emit email error if validation fails', () {
     mockValidation(value: 'error');
 
-    sut.emailErrorStream.listem(expectAsinc1((error) => expect(error, 'error')));
-    sut.isFormValidStream.listem(expectAsinc1((isValid) => expect(isValid, 'false')));
+    sut.emailErrorStream.listem(expectAsync1((error) => expect(error, 'error')));
+    sut.isFormValidStream.listem(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.ValidateEmail(email);
+    sut.ValidateEmail(email);
+
+  });
+
+  test('Should emit null if validation succeeds', () {
+    sut.emailErrorStream.listem(expectAsync1((error) => expect(error, null)));
+    sut.isFormValidStream.listem(expectAsync1((isValid) => expect(isValid, false)));
 
     sut.ValidateEmail(email);
     sut.ValidateEmail(email);
